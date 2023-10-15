725cd13cc7920f1263faa07a80b49c6e503c8597
BaoNewbie128 <binkongu24@gmail.com>
2023-10-15 17:22:51 +0700

    chapter 3 video3

--- copy2/checking.js
+++
@@ -1,3 +0,0 @@
-console.log('hello world from javascript')
-console.log('hello world from Paw')
-
---
+++ video3/video3.js
@@ -0,0 +1,5 @@
+console.log(`hello' '"' world from javascript:`)
+let x = 5,y=6,z=7;
+
+console.log("test string",'x=',x,'y=',y,'z=',z)
+// console.log("test string 2:",+'x='+ x +'y='+y+'z='+z)
